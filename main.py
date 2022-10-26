ce873db628c75ab90864387f2a739108f62c3f3c
tropicbliss <tropicbliss@protonmail.com>
2022-10-26 14:42:40 +0800

    More tests

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ class Msg(BaseModel):
     urls: list[str]
 
 
+@app.get("/")
+async def hello(inp: Msg):
+    return {"message": "bonjour!"}
+
+
 @app.post("/")
 async def root(inp: Msg):
     return {"message": inp.urls[0]}
