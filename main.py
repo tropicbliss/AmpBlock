718899139c496eb5f446aa7ceed379887c8874e8
tropicbliss <tropicbliss@protonmail.com>
2022-10-26 14:54:39 +0800

    Test

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
 
 class Msg(BaseModel):
-    msg: str
+    msg: List[str]
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+@app.post("/")
+async def root(inp: Msg):
+    return {"message": inp.msg[0]}
